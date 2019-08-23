0e45a8d6f91ffb4732ab21c08ca1b6a847dbab63
Edward Ezekiel <ed@ubiquisoft.com>
2019-08-23 14:19:43 -0500

    save name in local storage

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 // import firebase from 'firebase'
 
 const db = firebase.firestore();
-db.collection('players').get().then(resp => console.log(resp.docs[0]._document.data.internalValue.root.value.internalValue))
+db.collection("players")
+  .get()
+  .then(resp =>
+    console.log(
+      resp.docs[0]._document.data.internalValue.root.value.internalValue
+    )
+  );
 
 // get()
 // .then(resp => console.log(resp))
+
+const form = document.querySelector(".signInForm");
+form.onsubmit = submit;
+
+function submit(event) {
+  event.preventDefault();
+  const name = document.querySelector(".loginInput");
+  localStorage.setItem("Name", name.value);
+  console.log(localStorage.getItem("Name"))
+}
