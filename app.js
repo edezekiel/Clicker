63bca76dc19f325096d33896996b26356348babf
Edward Ezekiel <ed@ubiquisoft.com>
2019-08-23 13:36:20 -0500

    tweak the app.js file

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // import firebase from 'firebase'
 
 const db = firebase.firestore();
-db.collection('players').get()
-.then(resp => console.log(resp))
+db.collection('players').get().then(resp => console.log(resp.docs[0]._document.data.internalValue.root.value.internalValue))
+
+// get()
+// .then(resp => console.log(resp))
